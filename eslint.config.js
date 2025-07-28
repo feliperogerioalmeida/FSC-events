eb52b64ec87f6d03242790401be4a31541f11bee
Felipe Almeida <felipe.rogerio@outlook.com>
2025-07-28 09:05:12 -0300

    feat: add express setup with hello word route

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+])
